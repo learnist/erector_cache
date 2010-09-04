29c52bec984b24cf3d70ca48767be8ccdb26dbad
Shane Wolf <shanewolf@gmail.com>
2010-09-03 18:03:35 -0700

    cache key implementation

--- lib/erector_cache/widget.rb
+++ lib/erector_cache/widget.rb
@@ -2,19 +2,50 @@ module ErectorCache
   module Widget
     def self.included(base)
       base.extend ClassMethods
-      include InstanceMethods
     end
 
     module ClassMethods
       def cache_with(*components)
         class_inheritable_array :key_components
+        include InstanceMethods
         self.key_components = components
+      end
+      
+      def cache_key(hash)
+        self.key_components.inject([self.to_s]) do |collection, part|
+          object = part.is_a?(Hash) ? hash[part.keys.first] : hash[part]
+
+          if object.respond_to?(:to_param)
+            if part.is_a?(Hash)
+              key = part.keys.first
+              value = Array(part[key]).map do |p| 
+                if p.is_a?(Proc)
+                  p.call(object)
+                else
+                  object.send(p)
+                end
+              end.join("-")
+            else
+              key = part
+              value = object.to_param
+            end
+            collection << [key, value]
+          else
+            collection << [part, object]
+          end
+          collection
+        end.flatten.join(":")
       end
     end
 
     module InstanceMethods
       def cache_key
-        
+        key_data = {}
+        self.class.key_components.each do |part|
+          part = part.keys.first if part.is_a?(Hash)
+          key_data[part] = self.instance_variable_get("@#{part}")
+        end
+        return self.class.cache_key(key_data)
       end
     end
   end
--- spec/erector_cache/widget_spec.rb
+++ spec/erector_cache/widget_spec.rb
@@ -1,21 +1,32 @@
 require 'spec_helper'
 
 class Turtle < Erector::Widget
-  cache_with :age
-end
-
-class NinjaTurtle < Turtle
   cache_with :name
 end
 
+class NinjaTurtle < Turtle
+  cache_with :weapon
+end
+
 describe ErectorCache::Widget do
-  describe ".cache_with" do
-    it "sets the attributes to cache instances of this widget with" do
-      Turtle.key_components.should == [:age]
+  describe "ClassMethods" do
+    describe ".cache_with" do
+      it "sets the attributes to cache instances of this widget with" do
+        Turtle.key_components.should == [:name]
+      end
+
+      it "inherits parent classes cache key components" do
+        NinjaTurtle.key_components.should == [:name, :weapon]
+      end
     end
-    
-    it "inherits parent classes cache key components" do
-      NinjaTurtle.key_components.should == [:age, :name]
+  end
+  
+  describe "InstanceMethods" do
+    describe "#cache_key" do
+      it "builds a simple cache key for an instance properly" do
+        widget = Turtle.new(:name => "Yertle")
+        widget.cache_key.should == "Turtle:name:Yertle"
+      end
     end
   end
 end
